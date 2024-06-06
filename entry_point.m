8ba88abf07c4c74391d51488bfe5095b00a050e2
Luna Razzaghipour <luna@xoria.org>
2024-06-06 20:05:56 +1000

    Move initialization into `NSApplicationDelegate`

--- entry_point.m
+++ entry_point.m
@@ -4,34 +4,7 @@
 @import simd;
 
 #include "main_view.m"
-
-static NSString *
-AppName(void)
-{
-	NSBundle *bundle = [NSBundle mainBundle];
-	return [bundle objectForInfoDictionaryKey:(__bridge NSString *)kCFBundleNameKey];
-}
-
-static NSMenu *
-CreateMenu(void)
-{
-	NSMenu *menuBar = [[NSMenu alloc] init];
-
-	NSMenuItem *appMenuItem = [[NSMenuItem alloc] init];
-	[menuBar addItem:appMenuItem];
-
-	NSMenu *appMenu = [[NSMenu alloc] init];
-	appMenuItem.submenu = appMenu;
-
-	NSString *quitMenuItemTitle = [NSString stringWithFormat:@"Quit %@", AppName()];
-	NSMenuItem *quitMenuItem = [[NSMenuItem alloc] initWithTitle:quitMenuItemTitle
-	                                                      action:@selector(terminate:)
-	                                               keyEquivalent:@"q"];
-
-	[appMenu addItem:quitMenuItem];
-
-	return menuBar;
-}
+#include "app_delegate.m"
 
 int
 main(void)
@@ -45,24 +18,8 @@ main(void)
 	{
 		[NSApplication sharedApplication];
 		[NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
-
-		NSApp.mainMenu = CreateMenu();
-
-		NSRect rect = NSMakeRect(100, 100, 1000, 700);
-
-		NSWindowStyleMask style = NSWindowStyleMaskTitled | NSWindowStyleMaskResizable |
-		                          NSWindowStyleMaskClosable |
-		                          NSWindowStyleMaskMiniaturizable;
-		NSWindow *window = [[NSWindow alloc] initWithContentRect:rect
-		                                               styleMask:style
-		                                                 backing:NSBackingStoreBuffered
-		                                                   defer:NO];
-
-		MainView *view = [[MainView alloc] initWithFrame:rect];
-		window.contentView = view;
-
-		[window makeKeyAndOrderFront:nil];
-		[NSApp activateIgnoringOtherApps:YES];
+		AppDelegate *appDelegate = [[AppDelegate alloc] init];
+		NSApp.delegate = appDelegate;
 		[NSApp run];
 	}
 }
